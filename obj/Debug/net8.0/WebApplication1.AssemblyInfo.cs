f407fefc62db702748efa02a74c7e1fd2617bcd5
jannepaasila-collab <jannepaasila@gmail.com>
2026-02-02 17:04:10 +0200

    Rename screenshots folder to lowercase

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb45bc77e78fe44fe7338460ab2a86a3b6004ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728fb0a9ebd93f2708830e5320f7fb15be6cb0f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
